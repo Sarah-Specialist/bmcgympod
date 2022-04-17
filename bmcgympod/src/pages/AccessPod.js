fb67ff6e2cb49def61b7b663b7c44c92b8ef9a8b
Sarah Specialist <sarah.specialist@gmail.com>
2022-04-17 12:05:34 +0800

    added accesspod and endsession

---
+++ bmcgympod/src/pages/AccessPod.js
@@ -0,0 +1,21 @@
+import './AccessPod.css';
+
+export default function AccessPod() {
+    return (
+        <div className="access-pod">
+            <h1>Access the Pod</h1>
+            <h2>Step One: When you arrive at the pod, perform your TraceTogether check-in</h2>
+            <img src="https://i.ibb.co/qJRJW2H/image-6487327-5.jpg"
+                alt="trace together"
+                height="350"
+                width="250"
+            />
+            <h2>Step Two: Press "Open Door" and enter after the beep</h2>
+            <img src="https://i.ibb.co/7CQhc1q/image-6487327-4.jpg"
+                alt="open door"
+                height="350"
+                width="250"
+            />
+        </div>
+    )
+}
---
+++ bmcgympod/src/pages/EndSession.js
@@ -0,0 +1,34 @@
+import './EndSession.css';
+
+export default function EndSession() {
+    return (
+        <div className="end-session">
+            <h1>End the Session</h1>
+            <h2>Step One: Take note of the time that your session ends</h2>
+            <img src="https://i.ibb.co/rymp3L1/image-6487327-6.jpg"
+                alt="ending session"
+                height="350"
+                width="250"
+            />
+            <h2>Step Two: Clean the equipment using disinfectant</h2>
+            <img src="https://blog.pushpedalpull.com/hubfs/o-CLEAN-GYM-EQUIPMENT-facebook.jpg"
+                alt="clean equipment"
+                height="350"
+                width="600"
+            />
+            <h2>Step Three: Check that you have collected all your belongings</h2>
+            <img src="https://i.ibb.co/rymp3L1/image-6487327-6.jpg"
+                alt="collect belongings"
+                height="350"
+                width="250"
+            />
+            <h2>Step Four: Press the "Door Exit" button by the side of the door</h2>
+            <img src="https://i.ibb.co/wprqPLP/exit.jpg"
+                alt="door exit"
+                height="350"
+                width="350"
+            />
+            <h2>Step Five: Ensure that the door closes behind you, and is not blocked by any object</h2>
+        </div>
+    )
+}
