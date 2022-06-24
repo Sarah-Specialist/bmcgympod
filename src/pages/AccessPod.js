595f08bcab812fddee199a0c4b6ffca217f7d748
Sarah Specialist <sarah.specialist@gmail.com>
2022-06-24 20:54:58 +0800

    access pod images

--- src/pages/AccessPod.js
+++ src/pages/AccessPod.js
@@ -4,14 +4,14 @@ export default function AccessPod() {
     return (
         <div className="access-pod">
             <h1>Access the Pod</h1>
-            <h2>Step One: When you arrive at the pod, perform your TraceTogether check-in</h2>
-            <img src="https://i.ibb.co/qJRJW2H/image-6487327-5.jpg"
-                alt="trace together"
+            <h2>Step One: Press "Open Door" and enter after the beep</h2>
+            <img src="https://i.ibb.co/s635rzN/Whats-App-Image-2022-06-24-at-8-51-35-PM.jpg"
+                alt="open door"
                 height="350"
                 width="250"
             />
-            <h2>Step Two: Press "Open Door" and enter after the beep</h2>
-            <img src="https://i.ibb.co/7CQhc1q/image-6487327-4.jpg"
+            <h2>Enjoy your session!</h2>
+            <img src="https://i.ibb.co/jHygbzW/Whats-App-Image-2022-06-24-at-8-51-36-PM.jpg"
                 alt="open door"
                 height="350"
                 width="250"
